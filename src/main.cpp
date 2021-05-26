d0fc1ddf63dc63d1e4605d27aaea1be82ab7b378
dj-ryan <davidryn6@gmail.com>
2021-05-26 14:43:04 -0500

    added current time function

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,13 @@
 
 #include <ESP8266WiFi.h>
 #include <base64.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+
+const long utcOffsetInSeconds = -21600; // utc offset
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 // network info
 const String ssid = "VirtualIncision-Internal";
@@ -11,55 +18,129 @@ const String networkPassword = "R0b0tsCutY0u";
 const String recipientEmail = "vipermtempalarm@gmail.com";
 const String recipientEmailPassword = "";
 
+// email sender account info
 const String smtpUserName = "vipermtempalarmsender@gmail.com";
 const String smtpPassword = "PNiXugnd0cYt";
-
 
 // email sender account info
 const String senderEmail = "vipermtempalarmsender@gmail.com";
 const String senderEmailPassword = "XXaJQnV2t3utL7bu";
 
-String server = "mail.smtp2go.com"; // The SMTP Server 
+String server = "mail.smtp2go.com"; // The SMTP Server
 
-struct Button {
+struct Button
+{
   uint8_t PIN;
   bool pressed;
 };
 
-struct Email {
+struct Email
+{
   String smtpUserName;
   String smtpPassword;
   String sender;
   String senderPassword;
   String recipient;
-
-
-
-
-
 };
 
 bool alarmActive = false;
 
-unsigned long pressTime = 0;
+unsigned long pressTime = 0; // time of current press
 
-unsigned long pressReleaseDelta = 2000; // in ms
+unsigned long pressReleaseDelta = 2000; // required press time to reset in ms
 
-Button alarmTrigger {2, false};
-Button alarmReset {3, false};
+// button declarations
+Button alarmTrigger{2, false};
+Button alarmReset{3, false};
 
-uint8_t ledPin = 4;
+uint8_t ledPin = 4; // led pin number
 
 WiFiClient espClient;
 
-void ICACHE_RAM_ATTR isr() {
+void ICACHE_RAM_ATTR isr()
+{
   alarmReset.pressed = !alarmReset.pressed;
-  if(alarmReset.pressed == true) {
+  if (alarmReset.pressed == true)
+  {
     pressTime = millis();
-  } else {
-    if(millis() - pressTime > pressReleaseDelta) {
+  }
+  else
+  {
+    if (millis() - pressTime > pressReleaseDelta)
+    {
       alarmActive = false;
     }
+  }
+}
+
+// function prototypes
+void connectToWifi();
+int emailResp();
+int sendAlarmEmail();
+void getCurrentTime();
+
+void setup()
+{
+
+  Serial.begin(9600);
+
+  Serial.println("Beginning...");
+
+  pinMode(alarmReset.PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(alarmReset.PIN), isr, CHANGE);
+
+  pinMode(alarmTrigger.PIN, INPUT_PULLUP);
+
+  pinMode(ledPin, OUTPUT);
+
+  connectToWifi();
+
+  Serial.println("connection to wifi successfull");
+
+  sendAlarmEmail();
+
+  getCurrentTime();
+
+}
+
+void loop()
+{
+
+  delay(1000);
+  Serial.println("checking...");
+
+  if (digitalRead(alarmTrigger.PIN) == LOW)
+  {
+
+    alarmActive = true;
+
+    // blink light wait for isr
+    while (alarmActive == true)
+    {
+      // D1-mini chip io pins can draw more then they can
+      // output so inverted wiring in order to make led brighter
+      Serial.println("blinking...");
+      digitalWrite(ledPin, LOW); // turn LED ON
+      delay(500);
+      digitalWrite(ledPin, HIGH); // turn LED OFF
+      delay(200);
+      digitalWrite(ledPin, LOW); // turn LED ON
+      delay(500);
+      digitalWrite(ledPin, HIGH); // turn LED OFF
+      delay(1200);
+    }
+
+    // arming blinking signal
+    for (uint8_t i = 0; i < 10; i++)
+    {
+      digitalWrite(ledPin, LOW);
+      delay(100);
+      digitalWrite(ledPin, HIGH);
+      delay(100);
+    }
+    digitalWrite(ledPin, LOW);
+    delay(2000);
+    digitalWrite(ledPin, HIGH);
   }
 }
 
@@ -69,12 +150,12 @@ int emailResp()
   byte readByte;
   int loopCount = 0;
 
-  while (!espClient.available()) 
+  while (!espClient.available())
   {
     delay(1);
     loopCount++;
-    // Wait for 20 seconds and if nothing is received, stop.
-    if (loopCount > 20000) 
+
+    if (loopCount > 20000) // Wait for 20 seconds and if nothing is received, stop.
     {
       espClient.stop();
       Serial.println(F("\r\nTimeout"));
@@ -97,7 +178,8 @@ int emailResp()
   return 1;
 }
 
-void connectToWifi() {
+void connectToWifi()
+{
   delay(10);
   Serial.println("");
   Serial.println("");
@@ -116,47 +198,42 @@ void connectToWifi() {
   Serial.println(WiFi.localIP());
 }
 
+int sendAlarmEmail()
+{
 
+  Email email{smtpUserName, smtpPassword, senderEmail, senderEmailPassword, recipientEmail};
 
-int sendAlarmEmail() {
-
-
-Email email {smtpUserName, smtpPassword, senderEmail, senderEmailPassword, recipientEmail};
-
-if (espClient.connect(server, 2525) == 1) 
+  if (espClient.connect(server, 2525) == 1)
   {
     Serial.println(F("connected"));
-  } 
-  else 
+  }
+  else
   {
     Serial.println(F("connection failed"));
     return 0;
   }
-  if (!emailResp()) 
+  if (!emailResp())
     return 0;
   //
   Serial.println(F("Sending EHLO"));
   espClient.println("EHLO www.example.com");
-  if (!emailResp()) 
+  if (!emailResp())
     return 0;
-  //
+  // only use if STARTTLS seccurity is used
   /*Serial.println(F("Sending TTLS"));
   espClient.println("STARTTLS");
   if (!emailResp()) 
   return 0;*/
-  //  
+  //
   Serial.println("Sending auth login");
   espClient.println("AUTH LOGIN");
-  if (!emailResp()) 
+  if (!emailResp())
     return 0;
-  //  
+  //
   Serial.println("Sending User");
-  // Change this to your base64, ASCII encoded username
-  /*
-  For example, the email address test@gmail.com would be encoded as dGVzdEBnbWFpbC5jb20=
-  */
+
   espClient.println(base64::encode(email.smtpUserName)); //base64, ASCII encoded Username
-  if (!emailResp()) 
+  if (!emailResp())
     return 0;
   //
   Serial.println("Sending Password");
@@ -165,29 +242,26 @@ if (espClient.connect(server, 2525) == 1)
   For example, if your password is "testpassword" (excluding the quotes),
   it would be encoded as dGVzdHBhc3N3b3Jk
   */
-  espClient.println(base64::encode(email.smtpPassword));//base64, ASCII encoded Password
-  if (!emailResp()) 
+  espClient.println(base64::encode(email.smtpPassword)); //base64, ASCII encoded Password
+  if (!emailResp())
     return 0;
-  
 
   String from = "MAIL From: " + email.sender;
   Serial.println(from);
   espClient.println(from);
-  if (!emailResp()) 
+  if (!emailResp())
     return 0;
 
   String to = "RCPT To: " + email.recipient;
   Serial.println(to);
   espClient.println(to);
-  if (!emailResp()) 
+  if (!emailResp())
     return 0;
-  
 
   Serial.println("Sending DATA");
   espClient.println("DATA");
-  if (!emailResp()) 
+  if (!emailResp())
     return 0;
-
 
   Serial.println(F("Sending email"));
   // change to recipient address
@@ -200,90 +274,37 @@ if (espClient.connect(server, 2525) == 1)
   espClient.println(F("Third line of the test e-mail."));
   //
   espClient.println(F("."));
-  if (!emailResp()) 
+  if (!emailResp())
     return 0;
   //
   Serial.println(F("Sending QUIT"));
   espClient.println(F("QUIT"));
-  if (!emailResp()) 
+  if (!emailResp())
     return 0;
   //
   espClient.stop();
   Serial.println(F("disconnected"));
   return 1;
+}
+
+void getCurrentTime() {
+  WiFiUDP ntpUDP;
+  NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
 
 
-  
+  timeClient.begin();
+
+  timeClient.update();
+
+  Serial.print(daysOfTheWeek[timeClient.getDay()]);
+  Serial.print(", ");
+  Serial.print(timeClient.getHours());
+  Serial.print(":");
+  Serial.print(timeClient.getMinutes());
+  Serial.print(":");
+  Serial.println(timeClient.getSeconds());
 }
 
 
 
-
-
-
-void setup() {
-
-  Serial.begin(9600);
-
-  Serial.println("Beginning...");
-
-  pinMode(alarmReset.PIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(alarmReset.PIN), isr, CHANGE);
-  
-  pinMode(alarmTrigger.PIN, INPUT_PULLUP);  
-  
-  pinMode(ledPin, OUTPUT);
-  
-
-
-  connectToWifi();
-
-  Serial.println("connection to wifi successfull");
-
-  sendAlarmEmail();
-
-}
-
-void loop() {
-
-  delay(1000);
-  Serial.println("checking...");
-
-  if(digitalRead(alarmTrigger.PIN) == HIGH) {
-    
-    alarmActive = true;
-    
-    
-      
-  
-    // blink light wait for isr
-      while(alarmActive == true) {
-        // D1-mini chip io pins can draw more then they can 
-        // output so inverted wiring in order to make led brighter
-        Serial.println("blinking...");
-        digitalWrite(ledPin, LOW);  // turn LED ON
-        delay(500);
-        digitalWrite(ledPin, HIGH);  // turn LED OFF
-        delay(200);
-        digitalWrite(ledPin, LOW);  // turn LED ON
-        delay(500);
-        digitalWrite(ledPin, HIGH);  // turn LED OFF
-        delay(1200);
-    }
-
-
-    // arming blinking signal
-    for(uint8_t i = 0; i < 10; i++) {
-      digitalWrite(ledPin, LOW);
-      delay(100);
-      digitalWrite(ledPin, HIGH);
-      delay(100);
-    }
-      digitalWrite(ledPin,LOW);
-      delay(2000);
-      digitalWrite(ledPin,HIGH);
-  }
-
-
-}
-
+// -eof
